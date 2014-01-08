bcf4d5ea8815d226e4c4f782b383e9e99f05d9de
Caleb Sylvest <caleb.sylvest@gmail.com>
2014-01-08 17:15:08 -0600

    working on sermons markup

--- content-sermon.php
+++ content-sermon.php
@@ -78,11 +78,68 @@ HTML;
 	}
 
 ?>
-  <!-- a single sermon page -->
+  
 	<article id="post-<?php the_ID(); ?>" <?php post_class( 'uplifted-entry-full uplifted-sermon-full' ); ?>>
     
-		<?php get_template_part( 'content-sermon-header' ); ?>
-
+    <!-- a working Header containing Sermon meta, will be changing -->
+    <header class="uplifted-entry-header uplifted-clearfix">
+  
+    	<div class="uplifted-entry-title-meta">
+    
+    		<?php if ( ctfw_has_title() ) : ?>
+      		<h1 class="uplifted-entry-title<?php if ( is_singular( get_post_type() ) ) : ?> uplifted-main-title<?php endif; ?>">
+      			<?php uplifted_post_title(); // will be linked on short ?>
+      		</h1>
+      	<?php endif; ?>
+    
+    		<ul class="uplifted-entry-meta">
+    
+    			<li class="uplifted-entry-date uplifted-content-icon">
+    				<span class="<?php uplifted_icon_class( 'entry-date' ); ?>"></span>
+    				<time datetime="<?php esc_attr( the_time( 'c' ) ); ?>"><?php ctfw_post_date(); ?></time>
+    			</li>
+    
+    			<?php if ( $speakers = get_the_term_list( $post->ID, 'ctc_sermon_speaker', '', __( ', ', 'uplifted' ) ) ) : ?>
+    				<li class="uplifted-entry-byline uplifted-sermon-speaker uplifted-content-icon">
+    					<span class="<?php uplifted_icon_class( 'sermon-speaker' ); ?>"></span>
+    					<?php echo $speakers; ?>
+    				</li>
+    			<?php endif; ?>
+    
+    			<?php if ( $topics = get_the_term_list( $post->ID, 'ctc_sermon_topic', '', __( ', ', 'uplifted' ) ) ) : ?>
+    				<li class="uplifted-entry-category uplifted-sermon-topic uplifted-content-icon">
+    					<span class="<?php uplifted_icon_class( 'sermon-topic' ); ?>"></span>
+    					<?php echo $topics; ?>
+    				</li>
+    			<?php endif; ?>
+    
+    			<?php if ( $books = get_the_term_list( $post->ID, 'ctc_sermon_book', '', __( ', ', 'uplifted' ) ) ) : ?>
+    				<li class="uplifted-entry-category uplifted-sermon-book uplifted-content-icon">
+    					<span class="<?php uplifted_icon_class( 'sermon-book' ); ?>"></span>
+    					<?php echo $books; ?>
+    				</li>
+    			<?php endif; ?>
+    
+    			<?php if ( uplifted_show_comments() ) : ?>
+    				<li class="uplifted-entry-comments-link uplifted-content-icon">
+    					<span class="<?php uplifted_icon_class( 'comments-link' ); ?>"></span>
+    					<?php uplifted_comments_link(); ?>
+    				</li>
+    			<?php endif; ?>
+    
+    		</ul>
+    
+    	</div> <!-- /uplifted-entry-title-meta -->
+    	
+    	<?php if ( has_post_thumbnail() ) : ?>
+      	<div class="uplifted-entry-image">
+      		<?php uplifted_post_image(); ?>
+      	</div>
+      <?php endif; ?>
+   
+    </header>
+    <!-- a working Header containing Sermon meta, will be changing -->
+    
 		<?php
 		// Show media player and buttons only if post is not password protected
 		if ( ( $show_player || $show_buttons ) && ! post_password_required() ) :
@@ -174,7 +231,7 @@ HTML;
 
 				<?php endif; ?>
 
-			</div>
+			</div><!-- /uplifted-sermon-full-media -->
 
 		<?php endif; ?>
 
@@ -193,7 +250,7 @@ HTML;
 		<?php endif; ?>
 
 		<?php get_template_part( 'content-footer-full' ); // multipage nav, term lists, "Edit" button, etc. ?>
-
+  
 	</article>
 
 <?php
@@ -217,7 +274,9 @@ else :
 		<?php endif; ?>
 
 		<?php get_template_part( 'content-footer-short' ); // show appropriate button(s) ?>
-
+  
+    </div> <!-- /uplifted-content-meta -->
+  
 	</article>
 
 <?php endif; ?>
