c25b37ba354dede7025242506f71a98928163653
Chris Wallace <chris@liftux.com>
2014-03-17 09:29:45 -0500

    Fixes #19.

--- includes/support-wp.php
+++ includes/support-wp.php
@@ -30,18 +30,10 @@ function uplifted_add_theme_support_wp() {
 	// Add support for a variety of post formats
 	add_theme_support( 'post-formats', array( 'video', 'aside', 'link', 'gallery', 'status', 'quote', 'image' ) );
 
-	// Add support for custom backgrounds
-	add_theme_support('custom-background');
-
 	add_editor_style();
 
 	// This theme uses Featured Images (also known as post thumbnails) for per-post/per-page Custom Header images
 	add_theme_support( 'post-thumbnails' );
-
-	// Custom background with defaults
-	add_theme_support( 'custom-background', array(
-		'default-color' => '888888', // default color for Customizer (somewhere between light and dark)
-	) );
 
 }
 
